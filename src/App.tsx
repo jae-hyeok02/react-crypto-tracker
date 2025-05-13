d02ca057f311a9a4000756cc553be902246a3aad
jae-hyeok02 <kol5315@gmail.com>
2025-05-14 01:11:37 +0900

    fix: 초기 세팅 (패키지 설치 완료: React, styled-components, TS, react-router-dom, react-query)

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import styled, { keyframes } from "styled-components";
+import { useState } from "react";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <>
+      <Container>
+        <H1>protected</H1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
